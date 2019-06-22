d28c912f1476e269d40ddbb5834e6882c300b966
Hiromi Takahashi <hiromix222@gmail.com>
2019-06-22 17:32:59 +0900

    WebSecurityConfigurerAdapterを追加

---
+++ src/main/kotlin/com/example/omix222/kotlinbox/SecurityConfig.kt
@@ -0,0 +1,23 @@
+package com.example.omix222.kotlinbox
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+@Configuration
+@EnableWebSecurity
+
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(web: WebSecurity) {
+        // ここに設定したものはセキュリティ設定を無視
+        web.ignoring().antMatchers(
+            "/**/favicon.ico",
+            "/images/**",
+            "/css/**",
+            "/javascript/**",
+            "/webjars/**",
+            "/hello"
+        )
+    }
+
+}
